cf52daaec3882a77f7da4e7d95355cc1e09ee585
Melisa <soymelisa.pe.he@gmail.com>
2021-01-27 20:05:40 +0000

    Ejemplo pool p/ queries

---
+++ Ejemplo03.R
@@ -0,0 +1,13 @@
+install.packages("pool")
+install.packages("dbplyr")
+
+library(dbplyr)
+library(pool)
+
+my_db <- dbPool(
+  RMySQL::MySQL(), 
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest"
+)
